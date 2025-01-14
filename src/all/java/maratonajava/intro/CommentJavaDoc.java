31354933097564cd325cf3bdc4ef0273b624215b
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-01-13 22:51:42 -0300

    [maratona java] - intro package

---
+++ src/all/java/maratonajava/intro/CommentJavaDoc.java
@@ -0,0 +1,11 @@
+package all.java.maratonajava.intro;
+
+/**
+ * Exemplo de comentário Javadoc
+ */
+
+public class CommentJavaDoc {
+    public static void main(String[] args) {
+        System.out.println("Exemplo de comentário Java Doc");
+    }
+}
---
+++ src/all/java/maratonajava/intro/Hello.java
@@ -0,0 +1,7 @@
+package all.java.maratonajava.intro;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Maratona Java");
+    }
+}
