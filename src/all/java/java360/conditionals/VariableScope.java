e7c0d55fa3c8a17b532119343f0757cda2e6780b
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-01-15 22:48:21 -0300

    [java 360] - escopo de variaveis

---
+++ src/all/java/java360/conditionals/VariableScope.java
@@ -0,0 +1,22 @@
+package all.java.java360.conditionals;
+
+/**
+ * Area de atuação das variaveis
+ * value2 acessivel somente dentro da condicional
+ */
+public class VariableScope {
+    public static void main(String[] args) throws Exception {
+        int value = 10;
+
+        if(value == 10) {
+            int value2 = 20;
+        } else {
+            int value2 = 30;
+        }
+
+        int value2 = 40;
+
+        System.out.println(value2);
+
+    }
+}
