ce3ca85f52b9166bab76c6dd67ef1ee2536b381d
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-01-20 11:07:06 -0300

    [maratona java] - tipos primitivos

---
+++ src/all/java/maratonajava/intro/PrimitiveTypes.java
@@ -0,0 +1,27 @@
+package all.java.maratonajava.intro;
+
+/**
+ * Tipos primitivos
+ * int, double, float, char, byte, short, long, boolean
+ */
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        int age = 18;
+        double size = 12.1;
+        float price = 9;
+        char letter = 87;
+        byte bit = 127;
+        short number = 2;
+        long value = 10L;
+        boolean isTrue = true;
+
+        System.out.println("int:" + age);
+        System.out.println("double: " +size);
+        System.out.println("float: " +price);
+        System.out.println("char:" + letter);
+        System.out.println("bit:" + bit);
+        System.out.println("short: " + number);
+        System.out.println("long: " + value);
+        System.out.println("boolean:" + isTrue);
+    }
+}
