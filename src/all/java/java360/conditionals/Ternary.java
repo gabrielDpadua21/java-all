b57d06cef159a735fec93aa0ffe9dd42f2eeb6c0
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-01-20 10:53:14 -0300

    [java 360] - fim do modulo de condicionais

---
+++ src/all/java/java360/conditionals/Ternary.java
@@ -0,0 +1,13 @@
+package all.java.java360.conditionals;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int age = 18;
+        String text;
+
+        text = age >= 18 ? "of legal age" : "minor";
+
+        System.out.println(text);
+
+    }
+}
---
+++ src/all/java/java360/exercicies/WeekDayName.java
@@ -0,0 +1,30 @@
+package all.java.java360.exercicies;
+
+import all.java.java360.conditionals.Switch;
+
+import java.util.Scanner;
+
+public class WeekDayName {
+    public static void main(String[] args) {
+
+        int value;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write a number to see the week day name: ");
+        value = Integer.parseInt(sc.nextLine());
+
+        String weekName = switch (value) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Invalid day!";
+        };
+
+        System.out.println("is: " + weekName);
+
+    }
+}
