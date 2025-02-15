51935c08a7209e4a77a1de45477aff19871a2f41
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-02-15 02:36:37 -0300

    [maratona java] feat: ajuste nos tipos primitivos, função de verificação de tipos

--- src/all/java/maratonajava/intro/PrimitiveTypes.java
+++ src/all/java/maratonajava/intro/PrimitiveTypes.java
@@ -5,6 +5,11 @@ package all.java.maratonajava.intro;
  * int, double, float, char, byte, short, long, boolean
  */
 public class PrimitiveTypes {
+
+    public static <T> String printType(T obj, Class<T> clazz) {
+        return "Typeof is: " + clazz.getSimpleName();
+    }
+
     public static void main(String[] args) {
         int age = 18;
         double size = 12.1;
@@ -15,13 +20,13 @@ public class PrimitiveTypes {
         long value = 10L;
         boolean isTrue = true;
 
-        System.out.println("int:" + age);
-        System.out.println("double: " +size);
-        System.out.println("float: " +price);
-        System.out.println("char:" + letter);
-        System.out.println("bit:" + bit);
-        System.out.println("short: " + number);
-        System.out.println("long: " + value);
-        System.out.println("boolean:" + isTrue);
+        System.out.println(printType(age, Integer.class) + ", value: " + age);
+        System.out.println(printType(size, Double.class) + ",  value: " + size);
+        System.out.println(printType(price, Float.class) + ", value: " + price);
+        System.out.println(printType(letter, Character.class) + ", value: " + letter);
+        System.out.println(printType(bit, Byte.class) + ", value: " + bit);
+        System.out.println(printType(number, Short.class) + ", value: " + number);
+        System.out.println(printType(value, Long.class) + ", value: " + value);
+        System.out.println(printType(isTrue, Boolean.class) + ", value: " + isTrue);
     }
 }
