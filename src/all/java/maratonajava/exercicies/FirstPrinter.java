83e21782f17d6a2bbab3a7dd284c4aba4f7aa07a
Gabriel D. Pádua <gabriel.d.padua21@gmail.com>
2025-02-15 05:35:18 -0300

    [maratona java] feat: primeiro exercicio

---
+++ src/all/java/maratonajava/exercicies/FirstPrinter.java
@@ -0,0 +1,29 @@
+package all.java.maratonajava.exercicies;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class FirstPrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("type your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("type your address: ");
+        String address = sc.nextLine();
+
+        System.out.println("type the salary: ");
+        Double salary = Double.parseDouble(sc.nextLine());
+
+        System.out.println("type the date");
+        String date = sc.nextLine();
+
+        String report = """
+                I %s, living in %s, confirm to receive the salary of %s, in %s
+                """.formatted(name, address, salary, date);
+
+        System.out.println(report);
+
+    }
+}
